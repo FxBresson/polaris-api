e9dfcea695ae48599fcfa979e6eae67d3d63523e
FxBresson <fx-bresson@live.fr>
2019-04-10 21:35:45 +0200

    @Edit: Main router rewrite

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,29 @@
 import express from 'express';
 import passport from '../services/bnetStrategy.service'
 import { sendApiSuccessResponse } from '../services/response.service';
-import authRouter from './auth/auth.routes'
+import authRouter from './auth/auth.routes';
+import dataRouter from './data/data.routes';
+import graphlHTTP from 'express-graphql';
+import graphqlSchema from '../graphql/graphqlSchema';
+import { getUser, getUserLineup } from '../services/getContext.service';
 
 const mainRouter = express.Router();
 
-mainRouter.use('/auth', authRouter({passport}))
+mainRouter.use('/auth', authRouter({passport}));
+
+mainRouter.use('/data', dataRouter({passport}));
+
+mainRouter.use('/graphql', passport.authenticate('jwt', {session:false}), graphlHTTP((req, res, graphQLParams) => ({
+    schema: graphqlSchema,
+    graphiql: true,
+    rootValue: {
+        user: getUser(req),
+        lineupid: getUserLineup(req)
+    }
+})));
 
 mainRouter.get('/', (req, res) => {
-    apiResponse(res, 'Home');
+    return sendApiSuccessResponse(res, 'Home', {});
 });
 
 
